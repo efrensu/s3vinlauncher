60652382df354c8d81e5ce5f33036f98b17ff26b
efrensu <suareze96@gmail.com>
2024-09-23 23:19:20 -0700

    Create App.vue

---
+++ vue-files/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Applauncher from './components/Applauncher.vue';
+import Workapps from './components/Workapps.vue';
+import Pagenav from './components/Pagenav.vue';
+
+</script>
+
+<template>
+    <Navbar/>
+    <Applauncher/>
+    <Workapps/>
+    <Pagenav/>
+</template>
+
+<style>
+
+#app{
+  min-height: 100vh;
+  position: fixed;
+}
+
+body{
+  background-color: white;
+}
+
+
+
+</style>
